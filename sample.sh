3f644ed572165d688576c8e4341b625c04fdcc0c
chanduollala <82563584+chanduollala@users.noreply.github.com>
2023-05-25 21:17:31 +0530

    test

--- sample.sh
+++ sample.sh
@@ -11,14 +11,14 @@ elif [[ "$platform" == "Darwin" ]]; then
   is_macos=true
 fi
 
-
+echo "Checking if ruby is installed in system"
 # Check if Ruby is already installed
 if command -v ruby >/dev/null 2>&1; then
 	echo "Ruby found in the system"
 else
-		echo "Ruby not found! Installing..."
-    	if $is_linux; then
-    	  apt-get install ruby-full
-    	fi
+  echo "Ruby not found! Installing..."
+    if $is_linux; then
+      apt-get install ruby-full
+    fi
 fi
 
