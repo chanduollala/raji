0c90a0a7d745dcba13988fad3fea267295b0dbb2
chanduollala <82563584+chanduollala@users.noreply.github.com>
2023-05-25 21:05:54 +0530

    test

--- sample.sh
+++ sample.sh
@@ -1,26 +1,22 @@
+platform=$(uname)
+echo $platform
+if [[ "$platform" == "Linux" ]]; then
+  echo "Linux platform detected"
+else
+  echo "Unknown platform: $platform"
+fi
+
+
 echo "Hello chandu"
 #!/bin/bash
 echo "Hi"
 
 # Check if Ruby is already installed
-r_mode=0
 if command -v ruby >/dev/null 2>&1; then
-	r_mode=1
-else
-	if snap run ruby --version >/dev/null 2>&1; then
-		r_mode=2
-	fi
-fi
-echo $r_mode
-: '
-if [ "$r_mode" -eq 1 ]; then
 	echo "Ruby found in the system"
-elif [ "$r_mode" -eq 2 ]; then
-	echo "Ruby found as a snap"
-else
-	echo "Ruby not found! Installing..."
-	#sudo apt-get install ruby-full
-	sudo snap install ruby --classic
+elif snap run ruby --version >/dev/null 2>&1; then
+		echo "Ruby not found! Installing..."
+    	#sudo apt-get install ruby-full
+    	sudo snap install ruby --classic
 fi
-'
 
